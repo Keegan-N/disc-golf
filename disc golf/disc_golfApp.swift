a4c5c6738273cf05a3fca606e02d334513db6c53
keegan <letmeuseai66@gmail.com>
2024-10-10 12:45:21 -0700

    Initial Commit

---
+++ disc golf/disc_golfApp.swift
@@ -0,0 +1,17 @@
+//
+//  disc_golfApp.swift
+//  disc golf
+//
+//  Created by Norgard, Keegan - Student on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct disc_golfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
